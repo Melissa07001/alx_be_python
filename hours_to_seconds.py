ca2a5bab171f711d0bd53bea960ae560c0fd21ee
Melissa07001 <your_email@example.com>
2025-05-28 18:15:18 +0200

    Remove root python files to keep only python_introduction folder

--- hours_to_seconds.py
+++
@@ -1,6 +0,0 @@
-# hours_to_seconds.py
-
-hours = 2
-seconds = hours * 3600  # 3600 seconds in 1 hour
-
-print(f"{hours} hour(s) is {seconds} seconds.")
--- simple_interest.py
+++
@@ -1,7 +0,0 @@
-# simple_interest.py
-principal = 1000
-rate = 5
-time = 3
-
-interest = (principal * rate * time) / 100
-print(f"The simple interest is: {interest}")
