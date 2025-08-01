43f17144b5c829ef70728c36807dbfcf30fbb8f5
Melissa02 <132373164+Melissa07001@users.noreply.github.com>
2025-08-01 13:21:33 +0200

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
     'rest_framework',
+
+    # Your custom app
     'api',
 ]
+
 
 
 
