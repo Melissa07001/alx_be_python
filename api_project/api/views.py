d19cde8e0623e99aba060ac02aa67636e9b6fed3
Melissa02 <132373164+Melissa07001@users.noreply.github.com>
2025-08-01 13:26:13 +0200

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
-from django.http import JsonResponse
-
-def book_list(request):
-    books = [
-        {"id": 1, "title": "Book 1", "author": "Author A"},
-        {"id": 2, "title": "Book 2", "author": "Author B"},
-    ]
-    return JsonResponse(books, safe=False)
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
