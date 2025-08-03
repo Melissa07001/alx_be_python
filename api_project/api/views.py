3247888c24124c310aec2ec3138fb978ccb7f005
Melissa02 <132373164+Melissa07001@users.noreply.github.com>
2025-08-03 13:48:32 +0200

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,4 @@
-from rest_framework import generics
-from .models import Book
-from .serializers import BookSerializer
+from django.http import JsonResponse
 
-class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+def hello(request):
+    return JsonResponse({'message': 'Hello, ALX!'})
